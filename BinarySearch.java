7ec34436a8eefb8aa2f9c9c743e19d32f693a5fa
Juan-Pablo Velez <jpvelez@gmail.com>
2014-11-17 01:18:39 -0500

    Implement BinarySearch algorithm, whitelist filter application, and get supporting standard libraries and datasets

---
+++ BinarySearch.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class BinarySearch
+{
+	public static int rank(int key, int[] a)
+	{
+		// Array must be sorted.
+		int lo = a[0];
+		int hi = a.length - 1;
+		while (lo <= hi)
+		{
+			int mid = lo + (hi - lo) / 2;
+			if (key < a[mid]) hi = mid - 1;
+			else if (key > a[mid]) lo = mid + 1;
+			else return a[mid];
+		}
+		return -1;
+	}
+
+	public static void main(String[] args)
+	{
+		In in = new In(args[0]);
+		int[] whitelist = in.readAllInts();
+		Arrays.sort(whitelist);
+
+		while (!StdIn.isEmpty())
+		{	// Read key, print if not in whitelist.
+			int key = StdIn.readInt();
+			if (rank(key, whitelist) == -1)
+				StdOut.println(key);
+		}
+	}
+}
