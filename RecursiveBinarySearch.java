ae57b046295243231ecb0d3aa53fe1c58659e46d
Juan-Pablo Velez <jpvelez@gmail.com>
2014-11-18 00:13:53 -0500

    Print recursion depth of RecursiveBinarySearch as it runs

--- RecursiveBinarySearch.java
+++ RecursiveBinarySearch.java
@@ -2,15 +2,19 @@ public class RecursiveBinarySearch extends BinarySearch
 {
 	public static int rank(int key, int[] a)
 	{
-		return recursiveRank(key, a, 0, a.length - 1);
+		return recursiveRank(key, a, 0, a.length - 1, 0);
 	}
 
-	public static int recursiveRank(int key, int[] a, int lo, int hi)
+	public static int recursiveRank(int key, int[] a, int lo, int hi, int lvl)
 	{	
+		// Print recursion call arguments.
+		StdOut.println(lvl + " lo: " + lo + " hi: " + hi);
+		// Keep track of recursion depth.
+		lvl += 1; 
 		if (lo > hi) return -1;
 		int mid = lo + (hi - lo) / 2;
-		if 		(key < a[mid]) return recursiveRank(key, a, lo, mid - 1);
-		else if (key > a[mid]) return recursiveRank(key, a, mid + 1, hi);
+		if 		(key < a[mid]) return recursiveRank(key, a, lo, mid - 1, lvl);
+		else if (key > a[mid]) return recursiveRank(key, a, mid + 1, hi, lvl);
 		else 				   return mid;
 	}
 }
