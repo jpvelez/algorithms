2f461a46e1d9a128dcb4d7a0405d28a2118686c0
Juan-Pablo Velez <jpvelez@gmail.com>
2014-11-21 00:45:05 -0500

    Implement median finder algorithm, requires reading all values in memory and sorting them - or doing two passes of the data

---
+++ Median.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class Median
+{
+	public static double findMedian(double[] a)
+	{
+		int N = a.length;
+		int mid = (N - 1) / 2;	// Index of middle value.
+		if (N % 2 == 0)
+		{ // Even number of values.
+			return (a[mid] + a[mid + 1]) / 2;	// Take mean of middle values.
+		}
+		else
+		{ // Odd number of values.
+			return a[mid];
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		In in = new In(args[0]);
+		double[] doubles = in.readAllDoubles();
+
+		Arrays.sort(doubles); // Replace for custom sorting algorithm.
+		double median = findMedian(doubles);
+		StdOut.println("median: " + median);
+	}
+}
