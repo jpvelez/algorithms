a43513ec4569118a67f9827a6c43cec702fd160c
Juan-Pablo Velez <jpvelez@gmail.com>
2015-04-29 00:14:24 -0400

    Make Arithmetic Expression Evaluation Interpreter algorithm work with ResizedArrayStack

--- Evaluate.java
+++ Evaluate.java
@@ -1,30 +1,29 @@
-import Stack; // Change this once you implement it.
-
 public class Evaluate
 {
   public static void main(String[] args)
-    Stack<String> ops = new Stack<String>;
-    Stack<Double> vals = new Stack<Double>;
-    while (!StdIn.isEmpty?)
+  {
+    ResizingArrayStack<String> ops = new ResizingArrayStack<String>();
+    ResizingArrayStack<Double> vals = new ResizingArrayStack<Double>();
+    while (!StdIn.isEmpty())
     { // Read char from standard in, push if token.
       String s = StdIn.readString();
-      if      (s.equals('('))                ;
-      else if (s.equals('+'))     ops.push(s);
-      else if (s.equals('-'))     ops.push(s);
-      else if (s.equals('*'))     ops.push(s);
-      else if (s.equals('/'))     ops.push(s);
-      else if (s.equals('%'))     ops.push(s);
-      else if (s.equals('sqrt'))  ops.push(s);
-      else if (s.equals(')'))
-      { // If token equals ')', pop two vals, evaluate with op, push result.
+      if      (s.equals("("))                ;
+      else if (s.equals("+"))     ops.push(s);
+      else if (s.equals("-"))     ops.push(s);
+      else if (s.equals("*"))     ops.push(s);
+      else if (s.equals("/"))     ops.push(s);
+      else if (s.equals("%"))     ops.push(s);
+      else if (s.equals("sqrt"))  ops.push(s);
+      else if (s.equals(")"))
+      { // If token equals ")", pop two vals, evaluate with op, push result.
         String op = ops.pop();
-        double v = nums.pop();
-        if (op.equals('+'))         v = vals.pop() + v;
-        else if (op.equals('-'))    v = vals.pop() - v;
-        else if (op.equals('*'))    v = vals.pop() * v;
-        else if (op.equals('/'))    v = vals.pop() / v;
-        else if (op.equals('%'))    v = vals.pop() % v;
-        else if (op.equals('sqrt')) v = Math.sqrt(v);
+        double v = vals.pop();
+        if (op.equals("+"))         v = vals.pop() + v;
+        else if (op.equals("-"))    v = vals.pop() - v;
+        else if (op.equals("*"))    v = vals.pop() * v;
+        else if (op.equals("/"))    v = vals.pop() / v;
+        else if (op.equals("%"))    v = vals.pop() % v;
+        else if (op.equals("sqrt")) v = Math.sqrt(v);
         vals.push(v);
       } // If token not operator or paren, convert value to double and push.
       else {
